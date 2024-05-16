7eb4754a9bd9b2795b19a3d4c57fad5e59c9d036
Naman Singh <namannsingh1314@gmail.com>
2024-05-16 20:49:56 +0530

    hoja bhai please

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        Copyright <span>&#9400;</span> Naman Singh - All Rights Reserved.
+      </p>
+    </div>
+  );
+}
